a531da6ddfcbdd1f1c07164447be289de779462a
Ali <shabazali.ali@siemens.com>
2021-07-07 17:49:17 +0530

    day 1 assignment

---
+++ Day1-Assignment/Day1Assignment.js
@@ -0,0 +1,10 @@
+//Declaring a empty string
+let result = '';
+
+//Creating the desired output using string concatenation
+for (let i = 0; i < 5; i++) {
+  result += '* ';
+}
+
+//Logging the result
+console.log(result);
